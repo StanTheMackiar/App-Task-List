3b67705c4239ee5901139628bde868904de84774
Stanly Calle <callestanly@gmail.com>
2022-07-23 02:00:53 -0500

    Bug fixes
    • Fixed a bug with checkboxes input
    • The code has been optimized
    • Fixed visual bugs on mobile devices
    • Favicon added

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ let completedTaskCounter = 0; //CONTADOR DE TAREAS COMPLETADAS
 let editIsActive = false; // DEFINE SI ESTA ACTIVO EL MODO EDITAR O NO
 let template = null;
 let taskArray = []; // ARREGLO QUE ALMACENA EL VALOR DE CADA TAREA
-let taskEdit = null; // ELEMENTO HTML A EDITAR
-let callback = false;
-
+let editIndex = null;
 
 //MANEJO DEL DOM
 const taskInput = document.querySelector('#taskInput'); // INPUT DEL USUARIO
@@ -22,7 +20,6 @@ clearAll.setAttribute("id", "clearAll");
 clearAll.setAttribute("onclick", "clearAllTasks()");
 clearAll.innerHTML = "Borrar todas las tareas";
  
-// LOCAL STORAGE
 
 //EVENTOS
 //BOTONES
@@ -32,147 +29,181 @@ taskInput.addEventListener('keypress', addTask);
 // LLAMA A LA FUNCION QUE ITERA EL ARREGLO DEL LOCALSTORAGE
 
 refreshTasks()
-console.log(taskArray);
+
 
 //FUNCIONES
 
-//LOOP QUE GENERA LAS NOTAS GUARDADAS (LOCAL STORAGE)
+//MAP QUE GENERA LAS NOTAS GUARDADAS (LOCAL STORAGE)
 
 function refreshTasks(template) {
   if (localStorage.length != 0) {
-    secondSection.innerHTML = '';
+    cleanTaskArea()
     console.log(`LOCAL STORAGE ACTIVE`);
     taskArray = JSON.parse(localStorage.getItem("Tasks"))
-    console.log(`TaskCounter = ${taskArray.length}`);
+    completedTaskCounter = localStorage.getItem("Checks")
+    console.log(taskArray);
+    // MAPEA EL ARREGLO DEL LOCAL STORAGE
     taskArray.map(
-        (i, counter) => {
+        (task, i) => {
+            // COLOCA EL BOTON BORRAR TODO
             addClearButton();
-            console.log(`refreshTasks`);
-            counter++
+            // COLOCA LOS ELEMENTOS
             const newTask = document.createElement('div');
             newTask.setAttribute("class", "tasks");
-            newTask.setAttribute("id", `task${counter}`);
-            template = `<input type="checkbox" class="checkTask" id="checkTask${counter}" onchange = "addCompletedTasks(${counter})">
-            <label class="taskContent" id="editContent${counter}">${i}</label>
-            <img src="https://dl.dropbox.com/s/982dg86u2bkaqe2/edit.png?dl=0" alt="Editar" class="editButton" onclick=editTask(${counter})>
-            <img src="https://dl.dropbox.com/s/v9h96qxt4o78yzt/delete.png?dl=0" alt="Borrar" class="deleteButton" onclick="removeTask(${counter})">`; 
-            newTask.innerHTML = template;
+            newTask.setAttribute("id", `task${i}`);
+            if (task.isChecked === true) {
+                IsInputChecked = `<input type="checkbox" class="checkTask" id="checkTask${i}" onchange = "addCompletedTasks(${i})" checked>`
+            } else {
+                IsInputChecked = `<input type="checkbox" class="checkTask" id="checkTask${i}" onchange = "addCompletedTasks(${i})">`
+            }
+            template = `<label class="taskContent" id="editContent${i}">${task.taskContent}</label>
+            <img src="./img/edit.png" alt="Editar" class="editButton" onclick=editTask(${i})>
+            <img src="./img/delete.png" alt="Borrar" class="deleteButton" onclick="removeTask(${i})">`; 
+            newTask.innerHTML = IsInputChecked + template;
             secondSection.appendChild(newTask);
             
         }
     )
+    // ACTUALIZANDO ESTADISTICAS
     refreshData()
-    console.log(secondSection != 0);
   } 
 }
 
 
 // CREAR NUEVA TAREA
-function addTask (event, i) {
+function addTask (event, id) {
  // COMPROBANDO QUE NO SEA UN CAMPO VACIO
 if (taskInput.value != 0) {
        //COMPROBANDO TIPO DE EVENTO
     if (event.keyCode === 13 || event.type === "click") {
-        addClearButton();
-        if (editIsActive != true){
-            taskArray.push(taskInput.value)
-            console.log(`taskCounter = ${taskArray.length}`);
-            i = taskArray.length - 1;
+        if (editIsActive != true) {
+            // COLOCANDO ELEMENTOS EN EL ARREGLO PRINCIPAL
+            taskArray.push({
+                id: null,
+                taskContent: taskInput.value,
+                isChecked: false
+            })
+            // ORGANIZANDO LOS ID DE LOS ELEMENTOS
+            putIdToTasks()
+
+            // SELECCIONANDO ULTIMO OBJETO DEL ARRAY
+            console.log(taskArray);
+            id = taskArray[taskArray.length - 1].id;
             //CREANDO ELEMENTO DIV
             const newTask = document.createElement('div');
             // COLOCANDO ATRIBUTOS AL DIV
             newTask.setAttribute("class", "tasks");
-            newTask.setAttribute("id", `task${taskArray.length}`);
+            newTask.setAttribute("id", `task${taskArray[id].id}`);
             // PLANTILLA QUE CREA EL NUEVO ELEMENTO
-            template = `<input type="checkbox" class="checkTask" id="checkTask${taskArray.length}" onchange = "addCompletedTasks(${taskArray.length})">
-            <label class="taskContent" id="editContent${taskArray.length}">${taskArray[i]}</label>
-            <img src="https://dl.dropbox.com/s/982dg86u2bkaqe2/edit.png?dl=0" alt="Editar" class="editButton" onclick=editTask(${taskArray.length})>
-            <img src="https://dl.dropbox.com/s/v9h96qxt4o78yzt/delete.png?dl=0" alt="Borrar" class="deleteButton" onclick="removeTask(${taskArray.length})">`; 
+            template = `<input type="checkbox" class="checkTask" id="checkTask${taskArray[id].id}" onchange = "addCompletedTasks(${taskArray[id].id})">
+            <label class="taskContent" id="editContent${taskArray[id].id}">${taskArray[id].taskContent}</label>
+            <img src="./img/edit.png" alt="Editar" class="editButton" onclick=editTask(${taskArray[id].id})>
+            <img src="./img/delete.png" alt="Borrar" class="deleteButton" onclick="removeTask(${taskArray[id].id})">`; 
             // COLOCANDO EL CONTENIDO DE LA PLANTILLA EN EL NUEVO DIV
             newTask.innerHTML = template;
-
-            // COLOCANDO LOS VALORES DE LAS NOTAS EN EL ARREGLO PARA EL LOCAL STORAGE
-            
-            console.log(taskArray);
-            // ALMACENA LOS DATOS EN EL LOCAL STORAGE
-            localStorage.setItem("Tasks", JSON.stringify(taskArray));
-       
             //ANADIENDO ELEMENTO DIV
             secondSection.appendChild(newTask);
             taskInput.value = null;
-
+            
+            //ALMACENA LOS DATOS EN LOCAL STORAGE
+            addToLocalStorage();
             // ACTUALIZAR TOTAL TAREAS
             refreshData();
-            // FUNCION EDITAR TAREAS
+            // COLOCANDO BOTON DE BORRAR TODAS LAS TAREAS
+            addClearButton();
+            
             } else {
-                i = taskEdit - 1;
-                console.log(`i ${i}`);
-                taskArray[i] = taskInput.value;
-                document.querySelector(`#editContent${taskEdit}`).innerText = taskArray[i];
-                editIsActive = false;
-                taskInput.value = null;
-                console.log(taskArray);
-                localStorage.setItem("Tasks", JSON.stringify(taskArray));
+            // AÑADIR TAREA EN EL MODO EDITAR
+              addTaskAtEditMode(editIndex);
             }   
         }
 
         //ERROR AL NO INTRODUCIR NADA
     } else noInputError(event);
 };
-
-//ELIMINAR TAREA
-function removeTask(id, index) {
-    const removeTask = document.getElementById(`task${id}`);
-    checkTask = document.querySelector(`#checkTask${id}`);
-    secondSection.removeChild(removeTask);
-
-    // ELIMINAR TAREAS LOCAL STORAGE
-    index = id - 1; // ID SE RESTA PARA COINCIDIR CON EL INDEX DEL ARRAY
-    console.log(`index array ${index}`);
-    taskArray.splice(index, 1);
-    console.log(taskArray);
-    localStorage.setItem("Tasks", JSON.stringify(taskArray));
-    console.log(`LOCAL STORAGE DELETED`);
-  
-    
-    //CORREGIR TAREAS COMPLETADAS Y PENDIENTES
-    
-    if (checkTask.checked === true) {
-        completedTaskCounter--;
-    }
-    //ACTUALIZAR TOTAL TAREAS
-    refreshData();
-    refreshTasks();
+// EDITAR TAREAS
+function editTask(i) {
+    editIsActive = true;
+    taskInput.value = document.querySelector(`#editContent${i}`).textContent;
+    editIndex = i;
 };
 
-// TAREAS COMPLETADAS
-function addCompletedTasks(id) {
-    const checkTask = document.querySelector(`#checkTask${id}`);
-    console.log(checkTask.checked);
+ // AÑADIR TAREA EN EL MODO EDITAR
+ function addTaskAtEditMode(editIndex) {
+    taskArray[editIndex].taskContent = taskInput.value;
+    document.querySelector(`#editContent${editIndex}`).innerText = taskArray[editIndex].taskContent;
+    editIsActive = false;
+    taskInput.value = null;
+    console.log(taskArray);
+    addToLocalStorage();
+}   
+
+//ELIMINAR TAREA
+function removeTask(i) {
+    // OBTENIENDO ELEMENTOS A BORRAR
+    const removeTask = document.getElementById(`task${i}`);
+    const checkTask = document.querySelector(`#checkTask${i}`);
+    const confirmRemoveTask = confirm(`¿Desea borrar la tarea #${i + 1}?`);
+    // BORRANDO ELEMENTOS SI SE CONFIRMA EL AVISO
+    if (confirmRemoveTask) {
+    // ELIMINA EL ELEMENTO
+    secondSection.removeChild(removeTask);
+
+    // ELIMINAR TAREAS DEL LOCAL STORAGE
+    taskArray.splice(i, 1); //ELIMINANDO EL OBJETO DEL INDICE DEL ARRAY
+    addToLocalStorage();
+    
+
+    //ACTUALIZAR TAREAS COMPLETADAS 
+    if (checkTask.checked === true) { refreshCompleteTasksCounter(i) }
+    // REORGANIZANDO LOS ID DE LAS NOTAS
+    putIdToTasks();
+
+    // SI EL ARREGLO ES 0 BORRAR EL BOTON DE BORRAR TAREAS
+    if (taskArray.length === 0) {
+        deleteClearButton();
+    }
+    //ACTUALIZAR TOTAL TAREAS Y LOS ELEMENTOS
+    refreshData();
+    refreshTasks();
+}
+};
+
+
+// ACTUALIZAR TAREAS COMPLETADAS AL BORRAR ELEMENTO
+    function refreshCompleteTasksCounter(i) {
+        completedTaskCounter--;
+        addToLocalStorage();
+    }
+
+// CHECK DE LAS TAREAS COMPLETADAS
+function addCompletedTasks(i) {
+    const checkTask = document.querySelector(`#checkTask${i}`);
+    console.log(`CHECKED? ${checkTask.checked}`);
+    // SI ESTA CHECKEADO
     if (checkTask.checked === true) {
         completedTaskCounter++;
-    } if (checkTask.checked === false) {
-        completedTaskCounter--;
+        taskArray[i].isChecked = true;
+        console.log(taskArray);
+        addToLocalStorage()
+    // SI ESTA DESTILDEADO
+    } else {
+        completedTaskCounter--;  
+        taskArray[i].isChecked = false;
+        console.log(taskArray);
+        addToLocalStorage()
     }
-    // ACTUALIZAR TAREAS COMPLETADAS
+    // ACTUALIZAR ESTADISTICAS
     refreshData();
+    console.log(completedTaskCounter);
 };
 
 // ACTUALIZAR DATOS
 function refreshData() {
-    totalTasks.innerHTML = `Total de tareas: ${taskArray.length}`;
     completedTasks.innerHTML = `Tareas completadas: ${completedTaskCounter}`;
-    pendingTasks.innerHTML = `Tareas pendientes: ${taskArray.length - completedTaskCounter}`
+    pendingTasks.innerHTML = `Tareas pendientes: ${taskArray.length - completedTaskCounter}`;
+    totalTasks.innerHTML = `Total de tareas: ${taskArray.length}`;
 
-};
-
-// EDITAR TAREAS
-function editTask(id) {
-    editIsActive = true;
-    taskInput.value = document.querySelector(`#editContent${id}`).textContent;
-    taskEdit = id;
-    console.log(`tarea que se esta editando ${taskEdit}`);
-    console.log(`indice del arreglo que se esta editando ${taskEdit - 1}`);
 };
 
 // ERROR AL NO INTRODUCIR INPUT
@@ -186,20 +217,50 @@ function noInputError (event) {
     } 
 }
 
+// BOTON BORRAR TODAS LAS TAREAS
 function clearAllTasks (confirmClear) {
-    confirmClear = confirm(`Seguro que desea borrar todas sus tareas? esta accion no es resersible`);
+    confirmClear = confirm( `¿Seguro que desea borrar todas sus tareas? esta acción no es reversible.`);
     if (confirmClear) {
         taskArray = [];
-        localStorage.removeItem("Tasks");
+        removeLocalStorage();
         secondSection.innerHTML = '';
         completedTaskCounter = 0;
-        thirdSection.removeChild(clearAll);
+        deleteClearButton();
         refreshData();
     }
 
 }
-
+// AÑADIR BOTON DE BORRAR TODO
 function addClearButton() {
     thirdSection.appendChild(clearAll);
 
 }
+// ELIMINAR BOTON BORRAR TODO
+function deleteClearButton() {
+    thirdSection.removeChild(clearAll);
+}
+
+ //LIMPIA EL AREA DE TAREAS (SEGUNDA SECCION)
+function cleanTaskArea() {
+    secondSection.innerHTML = ''
+}
+
+    //COLOCAR DATOS EN LOCAL STORAGE
+function addToLocalStorage () {
+    localStorage.setItem("Checks", completedTaskCounter);
+    localStorage.setItem("Tasks", JSON.stringify(taskArray));
+    console.log(`LOCAL STORAGE SAVED`);
+}
+function removeLocalStorage() {
+    localStorage.removeItem("Tasks");
+    localStorage.removeItem("Checks");
+    console.log(`LOCAL STORAGE REMOVED`);
+}
+
+//COLOCANDO O REORGANIZANDO LOS ID DE LAS NOTAS
+function putIdToTasks() {
+    taskArray.map((task, i) => {
+        task.id = i;
+    })
+}
+
